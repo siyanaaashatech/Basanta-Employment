17690e67e189ca2ec024a637a962890774348cef
sujan <sujan.khanal0175@gmail.com>
2024-02-27 12:19:19 +0545

    fixes some issue in footer

--- resources/views/frontend/course.blade.php
+++ resources/views/frontend/course.blade.php
@@ -10,12 +10,11 @@
             <div class="row">
 
                 <div class="col-lg-8 col-md-8 col-sm-12 order-1 order-md-1">
-                    
-                            <img class="sample_page_image"
-                                src="{{ asset('uploads/course/' .$course->image) }}" alt="Country Image">
-                   
-                 
-                
+
+                    <img class="sample_page_image" src="{{ asset('uploads/course/' . $course->image) }}" alt="Country Image">
+
+
+
                 </div>
                 <div class="col-lg-12 col-md-12 col-sm-12 order-3 order-md-3 sample_page_content">
                     {!! $course->description !!}
@@ -26,9 +25,7 @@
                     <ul>
                         @foreach ($listcourse as $course)
                             <li>
-                                <a href="" >
-                                    {{ $course->title }}
-                                </a>
+                                <a href="{{ route('singleCourse', ['slug' => $course->slug]) }}">{{ $course->title }}</a>
                             </li>
                         @endforeach
                     </ul>
@@ -36,17 +33,4 @@
             </div>
         </div>
     </section>
-
-    {{-- <section class="related-posts">
-        <div class="container">
-            <h2>Related Posts</h2>
-            <ul>
-                @foreach ($relatedPosts as $relatedPost)
-                    <li>
-                        <a href="{{ route('singlePost', ['slug' => $relatedPost->slug]) }}">{{ $relatedPost->title }}</a>
-                    </li>
-                @endforeach
-            </ul>
-        </div>
-    </section> --}}
 @endsection
